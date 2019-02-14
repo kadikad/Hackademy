a0f88fa65861ab32cf0cfb7178aafdea4051168d
kadira.m <kadira.mujkanovic@student.unisg.ch>
2019-02-14 09:49:32 +0000

    add loops.py to git

---
+++ loops.py
@@ -0,0 +1,17 @@
+import code
+
+a_list = [1,2,3]
+
+another_list = ["harry", "ron", "hermoine"]
+yanother_list = [1,"harry",3.4,"blue"]
+
+
+the_sum = code.my_sum(a_list)
+print(the_sum)
+
+the_prod = code.my_prod(a_list)
+print(the_prod)
+
+the_count = code.my_count(a_list)
+print(the_count)
+
